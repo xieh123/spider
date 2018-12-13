968bb5b25ea2fb9a2f216b6ead7c00234f473806
xieh123 <1509145359@qq.com>
2018-12-13 15:48:13 +0800

    Update area.py

--- area.py
+++ area.py
@@ -14,24 +14,39 @@ def fetch_page(url):
     except RequestException:
         return None
 
+
 def parse_page(html):
-    # print(html)
-    pattern = re.compile('<td .*?>(\d+)</td>.*?<td .*?>(\S+)</td>',re.S)
-    items = re.findall(pattern,html)
+    pattern = re.compile('<td .*?>(\d+)</td>.*?<td .*?>(\S+)</td>', re.S)
+    items = re.findall(pattern, html)
+
+    parent = ''
+
     for item in items:
+        if item[0].strip()[3:] == '000':
+            tmp = ''
+            parent = item[0]
+        elif item[0].strip()[4:] == '00':
+            tmp = parent
+            parent = item[0]
+        else:
+            tmp = parent
+
         yield {
-            'value':item[0],
-            'name':item[1]
+            'value': item[0],
+            'name': item[1],
+            'parent': tmp
         }
 
+
 def write_to_file(content):
-    with open('result.json','a',encoding='utf-8') as f:
-        f.write(json.dumps(content,ensure_ascii=False) + '\n')
+    with open('result.json', 'a', encoding='utf-8') as f:
+        f.write(json.dumps(content, ensure_ascii=False) + '\n')
 
 
 url = 'http://www.mca.gov.cn/article/sj/xzqh/2018/201804-12/20181011221630.html'
 
-html = fetch_page(url)
-for item in parse_page(html):
-    write_to_file(item)
-
+if __name__ == '__main__':
+    html = fetch_page(url)
+    for item in parse_page(html):
+        write_to_file(item)
+        # print(item)
